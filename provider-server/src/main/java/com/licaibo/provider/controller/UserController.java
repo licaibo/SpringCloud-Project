e9da644e21462ca9c0003bd7b32a8527de5f613a
licaibo <871107274@qq.com>
2017-09-30 21:21:50 +0800

    增加服务提供者

--- provider-server/src/main/java/com/licaibo/provider/controller/UserController.java
+++ provider-server/src/main/java/com/licaibo/provider/controller/UserController.java
@@ -3,10 +3,7 @@ package com.licaibo.provider.controller;
 import com.licaibo.provider.bean.User;
 import com.licaibo.provider.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by licaibo on 2017/9/30
@@ -18,7 +15,7 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/{name}")
+    @GetMapping("/{name}")
     public User selectByName(@PathVariable String name) {
         return userService.selectByName(name);
     }
