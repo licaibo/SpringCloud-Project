199b627e791292387676a5489c2871ee27081e85
licaibo <871107274@qq.com>
2019-12-19 16:27:54 +0800

    工程配置

---
+++ auth-server/src/main/java/com/licaibo/auth/AuthServerApplication.java
@@ -0,0 +1,30 @@
+package com.licaibo.auth;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by licaibo on 2017/9/30
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class AuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServerApplication.class,args);
+    }
+
+    @LoadBalanced
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
