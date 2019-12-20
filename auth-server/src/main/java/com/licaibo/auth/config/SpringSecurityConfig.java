9419653e69cca4ca946838916ef8dc03fb8dd6ad
licaibo <871107274@qq.com>
2019-12-20 16:51:20 +0800

    spring security

---
+++ auth-server/src/main/java/com/licaibo/auth/config/SpringSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.licaibo.auth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * SpringSecurity 配置
+ * @author licaibo
+ * @date 2019-12-20
+ **/
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+
+
+}
