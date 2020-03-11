b9564bd95c19d5a70b2947693d6eefdc5beeb5b4
licaibo <871107274@qq.com>
2020-03-11 10:02:34 +0800

    基本工具类

--- framework-serve/src/main/java/com/licaibo/framework/enu/HttpResultEnum.java
+++ framework-serve/src/main/java/com/licaibo/framework/enu/HttpResultEnum.java
@@ -1,6 +1,7 @@
 package com.licaibo.framework.enu;
 
 /**
+ * 状态码枚举
  * @author licaibo
  * @date 2020-03-11
  **/
@@ -8,8 +9,8 @@ public enum HttpResultEnum {
 
     UNKONW_ERROR(-1, "未知错误"),
     SUCCESS(200, "成功"),
-    BAD_REQUEST(400, "错误请求"),
-    FORBIDDEN(403, "没有权限"),
+    BAD_REQUEST(400, "请求错误"),
+    FORBIDDEN(403, "权限错误"),
     FAIL(500,"服务器错误")
     ;
 
--- framework-serve/src/main/java/com/licaibo/framework/test.java
+++
@@ -1,8 +0,0 @@
-package com.licaibo.framework;
-
-/**
- * @author licaibo
- * @date 2020-03-11
- **/
-public class test {
-}
