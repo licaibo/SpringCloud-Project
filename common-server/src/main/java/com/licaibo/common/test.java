5ed0b18de8bc8b758c3a5b98be1e7f8f4c78e7de
licaibo <871107274@qq.com>
2020-03-21 16:45:29 +0800

    flowable工程搭建

---
+++ common-server/src/main/java/com/licaibo/common/test.java
@@ -0,0 +1,8 @@
+package com.licaibo.common;
+
+/**
+ * @author licaibo
+ * @date 2020-03-13
+ **/
+public class test {
+}
---
+++ flowable-server/src/main/java/com/licaibo/flowable/FlowableServerApplication.java
@@ -0,0 +1,18 @@
+package com.licaibo.flowable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Created by licaibo on 2017/9/30
+ */
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient
+public class FlowableServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FlowableServerApplication.class,args);
+    }
+}
